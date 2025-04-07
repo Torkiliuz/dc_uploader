4e89795e1d6600986221eb5dbbc65c03cc0edc32
Jeff Ma <jeffrey.ma95@gmail.com>
2025-04-06 20:21:27 -0400

    venv.path creation instead of wholescale start.sh replacement. start.sh now reads the venv path from venv.path and uses that.

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-VENV_PATH=/opt/dcc-uploader
-
 SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 
 cd "$SCRIPT_PATH" || exit
+
+VENV_PATH=$(head -n 1 venv.path)
 
 screen -dmS dcc-uploader "$VENV_PATH/bin/python3" app.py
