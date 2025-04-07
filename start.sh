aab37cb1bf31521e11ea7e2db62d732a30e38e59
Jeff Ma <jeffrey.ma95@gmail.com>
2025-04-06 20:24:51 -0400

    Add root/sudo check

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root or with sudo"
+    exit 1
+fi
+
 SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 
 cd "$SCRIPT_PATH" || exit
