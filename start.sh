7b2ac213edd36e141d159afaedc9baff43aa3fd5
Jeff Ma <jeffrey.ma95@gmail.com>
2025-04-09 15:23:46 -0400

    Greatly simplify install.sh by just hardcoding the venv to the project directory. There was really no need to install it outside, and allows for easier user uninstall of the program.

--- start.sh
+++ start.sh
@@ -8,6 +8,4 @@ SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 
 cd "$SCRIPT_PATH" || exit
 
-VENV_PATH=$(head -n 1 venv.path)
-
-screen -dmS dcc-uploader "$VENV_PATH/bin/python3" app.py
+screen -dmS dcc-uploader "venv/bin/python3" app.py
