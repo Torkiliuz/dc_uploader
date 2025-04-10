30c56d55e2ba9e02c91a61797be808342a91515c
Jeff Ma <jeffrey.ma95@gmail.com>
2025-04-10 15:44:34 -0400

    Add a config validator script

---
+++ utils/config_validator.sh
@@ -0,0 +1,65 @@
+print_help() {
+    echo "Script usage: $SCRIPT_NAME [OPTION]"
+    echo "Validates your config.ini to ensure the bare minimum arguments for uploading is met"
+    echo
+    echo "-h, --help: Show this help page"
+    exit 0
+}
+
+check() {
+    for REQUIRED_KEY in "${REQUIRED_KEYS[@]}"; do
+        if [[ "$1" == "$REQUIRED_KEY" ]]; then
+            if [[ -z "$2" ]]; then
+                if [ "$1" == "APIKEY" ]; then
+                    echo -e "${YELLOW}Warning${NOCOLOR}: TMDB API key not set, will not be able to get IMDB information"
+                elif [ "$1" == "CLIENT_ID" ] || [ "$1" == "CLIENT_SECRET" ]; then
+                    echo -e "${YELLOW}Warning${NOCOLOR}: IGDB $1 not set, will not be able to get video game information"
+                else
+                    echo -e "${RED}Fatal${NOCOLOR}: $1 must not be empty"
+                    FATAL_ERROR=true
+                fi
+            fi
+        fi
+    done
+}
+
+# Pretty colors
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NOCOLOR='\033[0m'
+
+FULL_SCRIPT_NAME=$(readlink -f "${BASH_SOURCE[0]}")
+SCRIPT_NAME="${FULL_SCRIPT_NAME##*/}"
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    print_help
+elif [ -n "$1" ]; then
+    echo "ERROR: Unrecognized argument" >&2
+    exit 1
+fi
+
+# Keys that require user to set them
+REQUIRED_KEYS=\
+("DATADIR" "USERNAME" "PASSWORD" "LOGINTXT" "APIKEY" "CLIENT_ID" "CLIENT_SECRET" "ETORFPATH" "WATCHFOLDER")
+FATAL_ERROR=false
+while IFS= read -r LINE; do
+    # Skip lines without an '=' sign
+    [[ "$LINE" != *=* ]] && continue
+
+    # Split into KEY and VALUE
+    KEY="${LINE%%=*}"
+    VALUE="${LINE#*=}"
+
+    # Trim whitespace
+    KEY="$(echo "$KEY" | sed -E 's/^[[:space:]]+|[[:space:]]+$//g')"
+    VALUE="$(echo "$VALUE" | sed -E 's/^[[:space:]]+|[[:space:]]+$//g')"
+
+    # Run the check
+    check "$KEY" "$VALUE"
+done < config.ini
+
+if $FATAL_ERROR; then
+    exit 1
+else
+    exit 0
+fi
